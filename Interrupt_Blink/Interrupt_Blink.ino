49a88a93550afce16fd0b86ce196f59f6b8fc0ba
Andrew Wallace <wallacan@mail.gvsu.edu>
2018-07-10 08:19:32 -0400

    adding more files

---
+++ Interrupt_Blink/Interrupt_Blink.ino
@@ -0,0 +1,44 @@
+/* 
+Example Timer1 Interrupt
+Flash LED every second
+*/
+
+#define ledPin 13
+int timer1_counter;
+int t1=0;
+int t2;
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  // Set timer1_counter to the correct value for our interrupt interval
+  //timer1_counter = 64911;   // preload timer 65536-16MHz/256/100Hz
+  timer1_counter = 64286;   // preload timer 65536-16MHz/256/50Hz
+  //timer1_counter = 34286;   // preload timer 65536-16MHz/256/2Hz
+  
+  TCNT1 = timer1_counter;   // preload timer
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+  interrupts();             // enable all interrupts
+}
+
+ISR(TIMER1_OVF_vect)        // interrupt service routine 
+{
+  t2 = millis();
+  TCNT1 = timer1_counter;   // preload timer
+  Serial.print(t2 - t1);
+  Serial.println("");
+  t1 = t2;
+  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);
+}
+
+void loop()
+{
+  // your program here...
+}
